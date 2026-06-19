67bfc92a2f3df7b14795b8630bc81a96b2415bc8
Amirmohamads1383 <161164840+Amirmohamads1383@users.noreply.github.com>
2026-06-19 18:10:36 +0330

    Fix Artist Section

---
+++ src/components/ArtistBox/ArtistBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default function ArtistBox({ title, img, id }) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <img
+        src={img}
+        alt={title}
+        className="w-50 h-50 object-cover rounded-full"
+      />
+      <h3 className="font-Pelak-Bold text-lg text-white">
+        <Link to={`/artist/${id}`}>{title}</Link>
+      </h3>
+    </div>
+  );
+}
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -3,6 +3,7 @@ import TitleHeader from "../../components/TitleHeader/TitleHeader";
 import HeroSwiper from "../../components/HeroSwiper/HeroSwiper";
 import RitmoPlus from "../../components/RitmoPlus/RitmoPlus"
 import TrendPlayList from "../../components/TrendPlayList/TrendPlayList"
+import ArtistBoxContainer from "../../components/ArtistBoxContainer/ArtistBoxContainer"
 
 export default function Home() {
   return (
@@ -19,6 +20,12 @@ export default function Home() {
             <RitmoPlus />
             <TrendPlayList />
           </div>
+        </div>
+      </section>
+      <section className="my-16">
+        <div className="container">
+          <TitleHeader title={"خواننده ترند"} button={true} arrow={true} />
+          <ArtistBoxContainer />
         </div>
       </section>
     </>
